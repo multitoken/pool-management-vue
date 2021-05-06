bc8415dd63879c94e548ecdbd16a4e15c5ed3a28
Vitaliy Idiatov <iadernik@gmail.com>
2021-05-06 12:55:37 +0500

    BSC support WIP

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,18 +1,38 @@
 import merge from 'lodash/merge';
 import registry from '@balancer-labs/assets/generated/pm/registry.homestead.json';
-import tokens from '@multitoken/assets/data/tokens.js';
-import homestead from '@/config/homestead.json';
+import tokensBSC from '@multitoken/assets/data/tokens-bsc.js';
+import tokensETH from '@multitoken/assets/data/tokens-eth.js';
+import mainnet from '@/config/homestead.json';
 import kovan from '@/config/kovan.json';
+import bsc from '@/config/bsc.json';
 import rinkeby from '@/config/rinkeby.json';
+import chainParams from '../helpers/chainParams.json';
 
 // Add tokens
 const registryKovan = {
   tokens: {},
   untrusted: []
 };
-for (const token of tokens) {
+for (const token of tokensETH) {
   registryKovan['tokens'][token.addresses.kovan] = {
     address: token.addresses.kovan,
+    id: token.coingeckoID,
+    name: token.name,
+    symbol: token.symbol,
+    decimals: token.decimals,
+    precision: 3,
+    hasIcon: false,
+    logoUrl: `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${token.addresses.main}/logo.png`
+  };
+}
+
+const registryBSC = {
+  tokens: {},
+  untrusted: []
+};
+for (const token of tokensBSC) {
+  registryBSC['tokens'][token.addresses.bsc] = {
+    address: token.addresses.bsc,
     id: token.coingeckoID,
     name: token.name,
     symbol: token.symbol,
@@ -41,11 +61,17 @@ const registryRinkeby = {
   untrusted: []
 };
 
-const configs = { homestead, kovan, rinkeby };
-configs.homestead = merge(registry, configs.homestead);
+const configs = { mainnet, kovan, bsc, rinkeby };
+configs.mainnet = merge(registry, configs.mainnet);
 configs.kovan = merge(registryKovan, configs.kovan);
+configs.bsc = merge(registryBSC, configs.bsc);
 configs.rinkeby = merge(registryRinkeby, configs.rinkeby);
-const network = process.env.VUE_APP_NETWORK || 'kovan';
+const walletChainId = Object.entries(chainParams).find(
+  p => p[1].chainId === window.ethereum?.chainId
+);
+walletChainId && console.log('walletChainId[0]:', walletChainId[0]);
+const network =
+  (walletChainId && walletChainId[0]) || process.env.VUE_APP_NETWORK || 'kovan';
 const config = configs[network];
 config.env = process.env.VUE_APP_ENV || 'staging';
 
